09b3deaa050f7f0e77869cc497b7c8644b8f274d
pomkalk <pomkalk@gmail.com>
2016-02-05 08:15:47 +0700

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,75 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>oops 404 for Website Template for free | Home :: w3layouts</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link href='https://fonts.googleapis.com/css?family=Bad+Script' rel='stylesheet' type='text/css'>
+<style type="text/css">
+body{
+	font-family: 'Bad Script', cursive;
+}
+body{
+	background:#eaeaea;
+}	
+.wrap{
+	margin:0 auto;
+	width:1000px;
+}
+.logo{
+	text-align:center;
+	margin-top:200px;
+}
+.logo img{
+	width:350px;
+}
+.logo p{
+	color:#272727;
+	font-size:40px;
+	margin-top:1px;
+}	
+.logo p span{
+	color:lightgreen;
+}	
+.sub a{
+	color:#fff;
+	background:#272727;
+	text-decoration:none;
+	padding:10px 20px;
+	font-size:13px;
+	font-family: arial, serif;
+	font-weight:bold;
+	-webkit-border-radius:.5em;
+	-moz-border-radius:.5em;
+	-border-radius:.5em;
+}	
+.footer{
+	color:black;
+	position:absolute;
+	right:10px;
+	bottom:10px;
+}	
+.footer a{
+	color:rgb(114, 173, 38);
+}	
+</style>
+</head>
+
+
+<body>
+ <div class="wrap">
+	<div class="logo">
+			<p>Ой - такая страница не найдена!</p>
+			<img src="{{ asset('img/404page.png') }}"/>
+			<div class="sub">
+			  <p><a href="{{ url('/') }}">Back </a></p>
+			</div>
+	</div>
+ </div>	
+	
+</body>

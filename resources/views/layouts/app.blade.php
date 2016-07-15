4ac320c000c3dcfd59bbe73ed571ea973c66360b
pomkalk <pomkalk@gmail.com>
2016-07-15 08:36:39 +0700

    added yandex metrika

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,5 +89,36 @@
 			$('#help-dialog').modal('setting','transition','fade up').modal('show');	
 		});
 	})
+
 </script>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter31659071 = new Ya.Metrika({
+                    id:31659071,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/31659071" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </html>
